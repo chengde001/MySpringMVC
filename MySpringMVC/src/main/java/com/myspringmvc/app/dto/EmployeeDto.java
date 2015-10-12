80bfc30ff2153fa1c7926f9ac30741275dbd3a21
chengde001 <chengde001@gmail.com>
2015-10-13 00:25:47 +0900

    2015/10/13 update spring version 3.1.1 to 4.1.1, Java1.6 to Java1.8

--- MySpringMVC/src/main/java/com/myspringmvc/app/dto/EmployeeDto.java
+++ MySpringMVC/src/main/java/com/myspringmvc/app/dto/EmployeeDto.java
@@ -1,10 +1,5 @@
 package com.myspringmvc.app.dto;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.NotEmpty;
-
 import lombok.Data;
 
 @Data
